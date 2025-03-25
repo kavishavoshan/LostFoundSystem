9d76dd64a6c53aa7aed52d4bf98be91bae0036d0
Thinal Dewnayana <thinaldilmith2002@gmail.com>
2025-03-25 23:08:32 +0530

    Add Header and Footer components; update routing in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import Main from "./pages/main/main";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -7,7 +8,8 @@ import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+      <Route path="/" element={<Main />} />
+      <Route path='/home' element={<Home/>} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
