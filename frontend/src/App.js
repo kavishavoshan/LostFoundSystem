a64edf46f13ebb6de16db99c926880be0ad08d47
kumesha02 <kumesha02@gmail.com>
2025-03-27 10:31:43 +0530

    resolved error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Messaging from './pages/Messaging';
 import Analytics from './pages/Analytics';
-import { Routes, Route } from "react-router-dom";
 import Main from "./pages/main/main";
 import Home from "./pages/Home";
 
