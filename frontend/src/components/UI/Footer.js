f33f675556a7a4531c8f2a1c901addfb559ccb44
Thinal Dewnayana <thinaldilmith2002@gmail.com>
2025-03-27 15:24:07 +0530

    user profile

--- frontend/src/components/UI/Footer.js
+++ frontend/src/components/UI/Footer.js
@@ -8,7 +8,7 @@ const Footer = ({ companyName = "Lost & Found System" }) => {
     <footer className="absolute bottom-0 w-full py-4 text-center text-gray-400 bg-slate-800">
       <div className="container px-4 mx-auto flex flex-col md:flex-row items-center justify-between">
         <div className="flex items-center space-x-6">
-          <img src={logo} alt="Company Logo" className="w-12 h-12" />
+          <img src={logo} alt="Company Logo" className="w-auto h-11" />
           <small>
             &copy; {currentYear} {companyName}. All rights reserved.
           </small>
