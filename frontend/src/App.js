44598b06c382572d01daba1b6f614a7381a9f3e4
Kavisha Voshan Wickramasinghe <kavishavoshan@gmail.com>
2025-03-27 10:03:18 +0530

    conflicts resolved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Messaging from './pages/Messaging';
 import Analytics from './pages/Analytics';
-=======
 import { Routes, Route } from "react-router-dom";
 import Main from "./pages/main/main";
 import Home from "./pages/Home";
@@ -15,7 +13,6 @@ import Register from "./pages/user/Register";
 import UserProfile from "./pages/user/Profile";
 
 import ProtectedRoute from "./components/ProtectedRoute";
->>>>>>> development
 
 //Footer
 import Footer from "./components/UI/Footer";
@@ -25,14 +22,10 @@ import AdminLogin from "./pages/admin/AdminLogin";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <Routes>
-      <Route path="/messages" element={<Messaging />} />
-      <Route path="/analytics" element={<Analytics />} />
-    </Routes>
-=======
     <>
       <Routes>
+        <Route path="/messages" element={<Messaging />} />
+        <Route path="/analytics" element={<Analytics />} />
         <Route path="/" element={<Main />} />
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -43,7 +36,6 @@ function App() {
       </Routes>
       <Footer />
     </>
->>>>>>> development
   );
 }
 
